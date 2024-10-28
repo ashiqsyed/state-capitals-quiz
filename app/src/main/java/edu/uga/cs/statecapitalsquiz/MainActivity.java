8c25d955e326b1bcd6deaed16d45b08899ed264a
ashiqsyed <ahs57816@uga.edu>
2024-10-28 18:31:14 -0400

    added csv reading functionality, added state capitals csv file to repository

--- app/src/main/java/edu/uga/cs/statecapitalsquiz/MainActivity.java
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.uga.cs.statecapitalsquiz;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.opencsv.CSVReader;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity.java";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        LinearLayout layout = findViewById(R.id.main);
+        try {
+            InputStream stream = getAssets().open("state_capitals.csv");
+            CSVReader csvReader = new CSVReader(new InputStreamReader(stream));
+            String[] nextRow;
+
+            while ((nextRow = csvReader.readNext()) != null) {
+                TextView tv = new TextView(getBaseContext());
+                tv.setText(nextRow[1]);
+                layout.addView(tv);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error reading CSV file");
+        }
     }
 }
