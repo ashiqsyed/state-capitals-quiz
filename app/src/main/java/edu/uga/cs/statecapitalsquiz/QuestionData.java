ddd514bcea66b775645c8ee607446763c77b6c10
ashiqsyed <ahs57816@uga.edu>
2024-10-29 18:08:01 -0400

    implemented functionality for retrieving information from questions table

---
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/QuestionData.java
@@ -0,0 +1,90 @@
+package edu.uga.cs.statecapitalsquiz;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class helps deal with fetching questions from the SQLite database
+ */
+public class QuestionData {
+    private final String TAG = "QuestionData.java";
+
+    private SQLiteDatabase db;
+    private SQLiteOpenHelper questionsDbHelper;
+    private static final String[] columns = {
+        QuizzesDBHelper.QUESTIONS_COLUMN_CAPITAL,
+        QuizzesDBHelper.QUESTIONS_COLUMN_ID,
+        QuizzesDBHelper.QUESTIONS_COLUMN_EXTRA1,
+        QuizzesDBHelper.QUESTIONS_COLUMN_EXTRA2,
+        QuizzesDBHelper.QUESTIONS_COLUMN_STATE,
+        QuizzesDBHelper.TABLE_QUESTIONS
+    };
+
+    public QuestionData(Context context) {
+        this.questionsDbHelper = QuizzesDBHelper.getInstance(context);
+    }
+
+    public void open() {
+        db = questionsDbHelper.getWritableDatabase();
+        Log.d(TAG, "Database opened");
+    }
+
+    public void close() {
+        if (questionsDbHelper != null) {
+            questionsDbHelper.close();
+            Log.d(TAG, "Database closed");
+        }
+    }
+
+    public boolean isDatabaseOpen() {
+        return db.isOpen();
+    }
+
+    public List<Question> getAllQuestions() {
+        ArrayList<Question> questions = new ArrayList<Question>();
+        Cursor c = null;
+        int column;
+
+        try {
+            c = db.query(QuizzesDBHelper.TABLE_QUESTIONS, columns, null, null, null, null, null);
+            if (c != null && c.getCount() > 0) {
+                while(c.moveToNext()) {
+                    column = c.getColumnIndex(QuizzesDBHelper.QUESTIONS_COLUMN_ID);
+                    long id = c.getLong(column);
+                    column = c.getColumnIndex(QuizzesDBHelper.QUESTIONS_COLUMN_CAPITAL);
+                    String capital = c.getString(column);
+                    column = c.getColumnIndex(QuizzesDBHelper.QUESTIONS_COLUMN_EXTRA1);
+                    String extra1 = c.getString(column);
+                    column = c.getColumnIndex(QuizzesDBHelper.QUESTIONS_COLUMN_EXTRA2);
+                    String extra2 = c.getString(column);
+                    column = c.getColumnIndex(QuizzesDBHelper.QUESTIONS_COLUMN_STATE);
+                    String state = c.getString(column);
+
+                    Question question = new Question(state, capital, extra1, extra2);
+                    question.setId(id);
+                    questions.add(question);
+
+                }
+            }
+            if (c != null) {
+                Log.d(TAG, "# of records from database is " + c.getCount());
+            } else {
+                Log.d(TAG, "# of records from database is 0");
+            }
+
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e);
+        } finally {
+            if (c != null) {
+                c.close();
+            }
+        }
+        return questions;
+    }
+}
